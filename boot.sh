7cf6af4381765ed7a32805b761a2d0fa73c47900
muhammadridho2301216-del <muhammad.ridho.2301216@students.um.ac.id>
2026-09-20 14:36:29 +0000

    uhuy

--- boot.sh
+++ boot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 PORT="${PORT:-8080}"
+TTYD_USERNAME="${TTYD_USERNAME:-server}"
+TTYD_PASSWORD="${TTYD_PASSWORD:-1802}"
 mkdir -p /workspace
 PERSISTENT_HOME=/workspace/.home/server
 mkdir -p "$PERSISTENT_HOME" "$PERSISTENT_HOME/.config" "$PERSISTENT_HOME/.local" "$PERSISTENT_HOME/.cache"
@@ -11,4 +13,4 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 export SHELL=/bin/bash
 cd /workspace
-exec gosu server /usr/bin/ttyd --port "$PORT" --interface 0.0.0.0 --writable tmux new -A -s main
+exec gosu server /usr/bin/ttyd --port "$PORT" --interface 0.0.0.0 --credential "$TTYD_USERNAME:$TTYD_PASSWORD" --writable tmux new -A -s main
